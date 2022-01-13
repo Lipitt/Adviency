db00e568c428e3f47dccca5c3589f956e484f810
lipitt <victor.david.lip@gmail.com>
2022-01-13 20:02:57 -0300

    agregada lista de regalos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [list, setList] = useState(["chocolate", "pan dulce", "juguetes"]);
   return (
     <div className="App">
       <h1>Regalos:</h1>
       <ul>
-        <li>...</li>
+        <li>
+          {list &&
+            list.map((item) => {
+              return <li>{item}</li>;
+            })}
+        </li>
       </ul>
     </div>
   );
