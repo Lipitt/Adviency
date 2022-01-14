01ca3edc1c083505acc305fae57f4813b739ba28
lipitt <victor.david.lip@gmail.com>
2022-01-14 18:24:41 -0300

    dia-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./styles.css";
 
+import { useState } from "react";
+
 export default function App() {
+  const [list, setList] = useState(["Chocolates", "Pan dulce", "Juguetes"]);
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="container">
+        <div className="regalos">
+          <h1>Regalos:</h1>
+
+          <ul>
+            {list &&
+              list.map((item, id) => {
+                return <li key={id}>{item}</li>;
+              })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
