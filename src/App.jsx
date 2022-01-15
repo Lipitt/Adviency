3bf4969ff8baf4c9eea67a4ef3aa25a421b50d0a
lipitt <victor.david.lip@gmail.com>
2022-01-15 09:24:58 -0300

    dia-3: agregamos funcionalidad para agregar regalos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [list, setList] = useState(["Chocolates", "Juguetes", "Libros"]);
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, text]);
+    setText("");
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="container">
+        <div className="regalos">
+          <h1>Regalos:</h1>
+          <ul>
+            {list &&
+              list.map((item, i) => {
+                return <li key={i}>{item}</li>;
+              })}
+          </ul>
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleTextChange}
+              type="text"
+              value={text}
+              placeholder="mas regalos..."
+            />
+            <button>Agregar regalo</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
