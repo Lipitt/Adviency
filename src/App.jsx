7d5d7d12608fa6add207b967d6647620d611b0af
lipitt <victor.david.lip@gmail.com>
2022-01-16 10:16:38 -0300

    deleted changes in main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,12 @@
 import "./styles.css";
-import { useState } from "react";
 
 export default function App() {
-  const [list, setList] = useState(["Chocolates", "Juegos", "Pan Dulce"]);
-  const [inputText, setInputText] = useState("");
-
-  const handleTextChange = (e) => {
-    setInputText(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setList([...list, inputText]);
-    setInputText("");
-  };
-
-  const borrarRegalo = (id) => {
-    setList(list.filter((item) => list.indexOf(item) !== id));
-  };
-
   return (
     <div className="App">
-      <div className="container">
-        <div className="regalos">
-          <form onSubmit={handleSubmit}>
-            <input
-              onChange={handleTextChange}
-              type="text"
-              placeholder="agrega mas reglaos..."
-              value={inputText}
-            />
-            <button type="submit">Agregar</button>
-          </form>
-
-          <h1>Regalos:</h1>
-          <ul>
-            {list &&
-              list.map((item, id) => {
-                return (
-                  <div key={id} className="regaloItem ">
-                    <li>{item}</li>
-                    <button
-                      className="btnDelete"
-                      onClick={() => borrarRegalo(id)}
-                    >
-                      X
-                    </button>
-                  </div>
-                );
-              })}
-          </ul>
-        </div>
-      </div>
+      <h1>Regalos:</h1>
+      <ul>
+        <li>...</li>
+      </ul>
     </div>
   );
 }
