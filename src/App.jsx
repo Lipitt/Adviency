edee2a5df2467c468b88ae505bbd8a5716a06846
lipitt <victor.david.lip@gmail.com>
2022-01-18 19:17:30 -0300

    dia-6 catching up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [list, setList] = useState(["Chocolates", "Juegos", "Pan Dulce"]);
+  const [inputText, setInputText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, inputText]);
+    setInputText("");
+  };
+
+  const handleTextChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleDeleteItem = (id) => {
+    setList(list.filter((item) => list.indexOf(item) !== id));
+  };
+
+  const handleDeleteAll = () => {
+    setList("");
+  };
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="agrega mas regalos..."
+            value={inputText}
+          />
+          <button>Agregar</button>
+        </form>
+        <h1>Regalos:</h1>
+        <ul>
+          {list &&
+            list.map((item, id) => {
+              return (
+                <div key={id}>
+                  <li>{item}</li>
+                  <button onClick={(id) => handleDeleteItem(id)}>X</button>
+                </div>
+              );
+            })}
+        </ul>
+        <button onClick={handleDeleteAll}>Borrar Todo</button>
+      </div>
     </div>
   );
 }
