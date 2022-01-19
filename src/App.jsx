dbbbc8da5d2a0afa8d074a853fab9e7d7334622d
lipitt <victor.david.lip@gmail.com>
2022-01-19 20:57:45 -0300

    dia-7: added validation on submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,69 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [list, setList] = useState(["Chocolates", "Juegos", "Pan Dulce"]);
+  const [inputText, setInputText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    !inputText || list.includes(inputText)
+      ? alert("El item no puede ser repetido ni vacio")
+      : setList([...list, inputText]);
+
+    setInputText("");
+  };
+
+  const handleTextChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleDeleteItem = (index) => {
+    setList(list.filter((item) => list.indexOf(item) !== index));
+  };
+
+  const handleDeleteAll = () => {
+    setList("");
+  };
+
   return (
     <div className="App">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>...</li>
-      </ul>
+      <div className="container">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleTextChange}
+              type="text"
+              value={inputText}
+              placeholder="Escribi tu regalo aca!"
+            />
+            <button>Agregar</button>
+          </form>
+
+          <h1>Regalos:</h1>
+          {list ? (
+            <ul>
+              {list.map((item, index) => {
+                return (
+                  <div className="regaloItem" key={index}>
+                    <li>{item}</li>
+                    <button
+                      onClick={() => handleDeleteItem(index)}
+                      className="btnDelete"
+                    >
+                      X
+                    </button>
+                  </div>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>Agrega regalos a la lista!</p>
+          )}
+          <button onClick={handleDeleteAll}>Borrar Todo</button>
+        </div>
+      </div>
     </div>
   );
 }
